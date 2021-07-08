630444a202c7705d586ec5bfefef05d45dd2fc72
Meghan Murphy <meghanmurphy.212@gmail.com>
2021-07-07 17:36:59 -0700

    wrote all prompts and added suggestion library

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const fs = require("fs");
 
 // const markDown = require("./utils/generateMarkdown");
 
+inquirer.registerPrompt('suggest', require('inquirer-prompt-suggest'));
+
 // TODO: Create an array of questions for user input
 const questions = [
     {
@@ -27,15 +29,41 @@ const questions = [
 
     {
         type: "input",
-        message: "Please write a short description of your project",
+        message: "Please write a short description of your project.",
         name: "description",
     },
 
     {
         type: "list",
-        message: "What kind of license should your project have",
+        message: "What kind of license should your project have?",
         name: "license",
         choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None"],
+    },
+
+    {
+        type: "suggest",
+        message: "What command should be run to install dependencies?",
+        name: "dependency",
+        suggestions: ["npm i", "npm install", "npm i --save"],
+    },
+
+    {
+        type: "suggest",
+        message: "What command should be run to run tests?",
+        name: "test",
+        suggestions: ["npm test", "test", "nothing"],
+    },
+
+    {
+        type: "input",
+        message: "What does the user need to know about using the repo?",
+        name: "use",
+    },
+
+    {
+        type: "input",
+        message: "What does the user need to know about contributing to the repo?",
+        name: "contributing",
     },
 ];
 
@@ -43,7 +71,7 @@ const questions = [
 const writeToFile = (answers) =>
         `## ${answers.title}
 ## Description
-    ${answers.description}
+   ${answers.description}
 ## Table of Contents
 ## Installation
 ## Usage
